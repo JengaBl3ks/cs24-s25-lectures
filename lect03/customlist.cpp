12e0c6e80e1c0844bdcf492e3678840e866483c2
Rahul Janga <rahuljanga@ucsb.edu>
2025-04-08 13:16:18 -0700

    Update customlist.cpp

--- lect03/customlist.cpp
+++ lect03/customlist.cpp
@@ -44,6 +44,14 @@ void createPlaylist(){
 
 
 
+ CustomList list;
+    list.add("Bad");
+    list.add("Beat It");
+    list.add("Thriller");
+    Node* temp = list.head;
+    while (temp=temp->next){
+        cout << temp->value;
+    }
 
 
 
@@ -68,4 +76,4 @@ void createPlaylist(){
 int main(){
     createPlayList();
     return 0;
-}
+}}
